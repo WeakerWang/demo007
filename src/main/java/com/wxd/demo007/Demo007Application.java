fe3df9f10d00893eaaa76c818497d87964a661b3
WeakerWang <943384220@qq.com>
2023-05-19 15:05:13 +0800

    111

--- src/main/java/com/wxd/demo007/Demo007Application.java
+++ src/main/java/com/wxd/demo007/Demo007Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.wxd.demo007.mapper")
+//@MapperScan("com.wxd.demo007.mapper")
 public class Demo007Application {
 
     public static void main(String[] args) {
--- src/main/java/com/wxd/demo007/service/impl/WorkerServiceImpl.java
+++ src/main/java/com/wxd/demo007/service/impl/WorkerServiceImpl.java
@@ -9,10 +9,7 @@ import com.wxd.demo007.service.IWorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * <p>
