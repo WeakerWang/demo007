32e084b855d6c9372fda7876782971ccc4e4cf80
WeakerWang <943384220@qq.com>
2023-05-19 15:13:14 +0800

    22

--- src/main/java/com/wxd/demo007/Demo007Application.java
+++ src/main/java/com/wxd/demo007/Demo007Application.java
@@ -1,11 +1,9 @@
 package com.wxd.demo007;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@MapperScan("com.wxd.demo007.mapper")
 public class Demo007Application {
 
     public static void main(String[] args) {
